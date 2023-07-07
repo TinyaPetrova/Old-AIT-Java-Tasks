68dc17b79e709642dd38cc8314f3009366865c2d
Ustinia <98020199+TinyaPetrova@users.noreply.github.com>
2023-07-07 17:39:31 +0200

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,34 @@
+package lesson43;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Task1 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    try {
+      System.out.print("Введите размер списка: ");
+      int size = scanner.nextInt();
+      scanner.nextLine();
+
+      List<String> names = new ArrayList<>();
+      for (int i = 0; i < size; ++i) {
+        String name = scanner.nextLine();
+        names.add(name);
+      }
+
+      System.out.println("Введите номер имени для вывода: ");
+      int index = scanner.nextInt();
+      String selectedName = names.get(index - 1);
+      System.out.println("Выбранное имя: " + selectedName);
+
+    } catch (InputMismatchException e) {
+      System.out.println("Ошибка ввода: нужен номер имени");
+    } catch (IndexOutOfBoundsException e) {
+      System.out.println("Такого номера нет");
+    }
+  }
+}
