84abca25ebe930e0b8a6b410bb4351c40333fa00
Ustinia <98020199+TinyaPetrova@users.noreply.github.com>
2023-07-14 18:34:18 +0200

    Update Task1.java

--- Lesson45_1/Task1.java
+++ Lesson45_1/Task1.java
@@ -15,20 +15,17 @@ import java.util.Set;
  */
 
 public class Task1 {
+  
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     System.out.print("Введите кол-во чисел: ");
     int count = scanner.nextInt();
-
     Set<Integer> numbers = new HashSet<>();
-
     System.out.println("Введите числа, каждое с новой строки: ");
     for (int i = 0; i < count; i++) {
       int number = scanner.nextInt();
       numbers.add(number);
     }
-
     System.out.println("Полученное множество: ");
     for (int number : numbers) {
       System.out.println(number);
