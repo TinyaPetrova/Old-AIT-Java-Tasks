2460129319408436c465d7c31192af3295ce8c94
Ustinia <98020199+TinyaPetrova@users.noreply.github.com>
2023-07-14 18:33:45 +0200

    Update Task1.java

--- Lesson46_1/Task1.java
+++ Lesson46_1/Task1.java
@@ -4,23 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+Вам дан словарь, состоящий из пар слов. Каждое слово является синонимом к парному ему слову. Все слова в словаре различны.
+
+Для слова из словаря, записанного в последней строке, определите его синоним.
+
+Формат ввода:
+в первой строке - количество записей в словаре
+в следующих строках - сами записи: первое слово (ключ) и второе слово (синоним), каждое слово в отдельной строке
+в последней строке - слово, синоним для которого нужно вывести
+  */
 public class Task1 {
+  
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
     System.out.print("Введите кол-во записей в словаре: ");
     int numb = scanner.nextInt();
-
     Map<String, String> dictionary = new HashMap<>();
-
     for (int i = 0; i < numb; ++i) {
       String key = scanner.next();
       String synonym = scanner.next();
       dictionary.put(key, synonym);
     }
-
     String word = scanner.next();
-
     String synonym = dictionary.get(word);
 
     if (synonym != null) {
