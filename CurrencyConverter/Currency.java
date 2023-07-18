0bc3d30a39d68ea0d9bc8c901e49e705dae66b47
Ustinia <98020199+TinyaPetrova@users.noreply.github.com>
2023-07-18 19:14:17 +0200

    Update Currency.java

--- CurrencyConverter/Currency.java
+++ CurrencyConverter/Currency.java
@@ -2,3 +2,13 @@ public class Currency {
   private String currencyName;
   // тут просто пропишем конструктор, геттеры и сеттеры
 }
+
+@Override
+    public int hashCode() {
+        // переопределяем
+    }
+
+@Override
+    public boolean equals() {
+        // переопределяем
+    }
